4290dd13c9954a931a34c2aa405f78a13dd401a3
Dandev0 <99640203+Dandev0@users.noreply.github.com>
2022-11-18 21:39:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ async def handler_post(request):
 
 def main():
     app.add_routes(routes)
-    web.run_app(app, host='http://rekruto.io/', port=8080)
+    web.run_app(app, host='https://dandev0.github.io/test_task_rekruto/', port=8080)
 
 
 if __name__ == '__main__':
