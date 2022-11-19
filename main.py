e5c8519623881e2fbf49680441c0628ca095b80f
Dandev0 <99640203+Dandev0@users.noreply.github.com>
2022-11-19 17:32:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
 from aiohttp import web
-import aiohttp_jinja2
-import jinja2
 
 
 app = web.Application()
 routes = web.RouteTableDef()
 
 
-@routes.get('/')
-@aiohttp_jinja2.template('index.html')
+@routes.get('/rekruto')
 async def handler_get(request):
     name = request.rel_url.query['name']
     message = request.rel_url.query['message']
-    info = f'{name}, {message}'
-    return info 
-
-
-@routes.post('/')
-async def handler_post(request):
-    data = await request.json()
-    name = data['name']
-    message = data['message']
-    return web.Response(text=f"Hello {name}, {message}")
+    return web.Response(text=f'Привет {name}, {message}')
 
 
 def main():
